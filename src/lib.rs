b7ae022f2822c75a4a2ad3b60bcb4cf7bf640800
Bahtiar `kalkin` Gadimov <bahtiar@gadimov.de>
2022-01-17 14:31:35 +0100

    docs: Add licensing information

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+// Copyright (c) 2022 Bahtiar `kalkin` Gadimov <bahtiar@gadimov.de>
+//
+// This file is part of subject-classifier.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 //! Library for classifying a commit by it's subject
 use lazy_static::lazy_static;
 use regex::{Captures, Regex, RegexBuilder};
